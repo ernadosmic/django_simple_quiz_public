13cb677f01cdb9a147d3f8190ebc6143b830e0ce
Ernad Osmic <ernadosmic@hotmail.com>
2022-07-13 00:20:16 +0200

    commit

--- javna_uprava/views.py
+++ javna_uprava/views.py
@@ -1,7 +1,7 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.http import HttpResponse
-from .models import Question, Choice
+from .models import Question
 import random
 
 
