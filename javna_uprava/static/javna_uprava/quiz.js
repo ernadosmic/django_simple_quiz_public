352f8382f0dda24ebfc8d9f9de7d4571837d9ca2
Ernad Osmic <ernadosmic@hotmail.com>
2022-07-13 21:14:25 +0200

    commit

--- javna_uprava/static/javna_uprava/quiz.js
+++ javna_uprava/static/javna_uprava/quiz.js
@@ -1,28 +1,60 @@
-console.log(jsQuestionList)
+console.log(jsQuestionList);
+
 function* nextQuestion() {
-    let counter = 0
+    let counter = 0;
+    console.log(counter);
     for (i in jsQuestionList) {
-        document.getElementById("question_text_box").innerHTML =
-            jsQuestionList[i].question;
+        counter++;
+        console.log(counter);
+        console.log(jsQuestionList[i].question);
 
-        document.getElementById(`choice_1`).innerHTML =
-            Object.keys(jsQuestionList[i].choices[1]);
-        document.getElementById(`choice_2`).innerHTML =
-            Object.keys(jsQuestionList[i].choices[2]);
-        document.getElementById(`choice_3`).innerHTML =
-            Object.keys(jsQuestionList[i].choices[3]);
-        document.getElementById(`choice_4`).innerHTML =
-            Object.keys(jsQuestionList[i].choices[4]);
-        document.getElementById(`choice_5`).innerHTML =
-            Object.keys(jsQuestionList[i].choices[5]);
-        document.getElementById(`choice_6`).innerHTML =
-            Object.keys(jsQuestionList[i].choices[6]);
+        // Try to remove current question box if it exists
+        try {
+            const elem = document.getElementById("current_question_box");
+            elem.parentNode.removeChild(elem);
+        } catch (error) { }
 
-        yield
+        // For every question create a div
+        const current_question_box = document.createElement("div");
+        current_question_box.setAttribute("id", "current_question_box");
+        document.getElementById("question_box").appendChild(current_question_box);
+
+        // Create question text inside a created div
+        const current_question_text = document.createElement("div");
+        current_question_text.setAttribute("id", "current_question_text");
+        current_question_text.textContent = jsQuestionList[i].question;
+        document
+            .getElementById("current_question_box")
+            .appendChild(current_question_text);
+
+        // Create current_choices_box
+        const current_choices_box = document.createElement("div");
+        current_choices_box.setAttribute("id", "current_choices_box");
+        document
+            .getElementById("current_question_box")
+            .appendChild(current_choices_box);
+
+        // Create choices buttons
+        for (x in jsQuestionList[i].choices) {
+            try {
+                const btn = document.createElement("button");
+                btn.setAttribute("id", `${i}_${x}`);
+                btn.setAttribute("onclick", "nextQuestionClick()");
+                btn.setAttribute("value", Object.values(jsQuestionList[i].choices[x]))
+                btn.textContent = Object.keys(jsQuestionList[i].choices[x]);
+                document.getElementById("current_choices_box").appendChild(btn);
+            } catch (error) { }
+        }
+
+        yield;
     }
-
 }
 
-const nextQ = nextQuestion()
-nextQ.next()
+const nextQ = nextQuestion();
+nextQ.next();
 
+async function nextQuestionClick() {
+
+    await
+        nextQ.next();
+}
