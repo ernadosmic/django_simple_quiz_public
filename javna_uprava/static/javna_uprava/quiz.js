cc06d6dc662228d5bb8ac965e99dd467a384f5c0
Ernad Osmic <ernadosmic@hotmail.com>
2022-07-13 18:21:30 +0200

    commit

--- javna_uprava/static/javna_uprava/quiz.js
+++ javna_uprava/static/javna_uprava/quiz.js
@@ -1,3 +1,28 @@
 console.log(jsQuestionList)
+function* nextQuestion() {
+    let counter = 0
+    for (i in jsQuestionList) {
+        document.getElementById("question_text_box").innerHTML =
+            jsQuestionList[i].question;
 
-document.getElementById("question_text_box").innerHTML = "Govno"
+        document.getElementById(`choice_1`).innerHTML =
+            Object.keys(jsQuestionList[i].choices[1]);
+        document.getElementById(`choice_2`).innerHTML =
+            Object.keys(jsQuestionList[i].choices[2]);
+        document.getElementById(`choice_3`).innerHTML =
+            Object.keys(jsQuestionList[i].choices[3]);
+        document.getElementById(`choice_4`).innerHTML =
+            Object.keys(jsQuestionList[i].choices[4]);
+        document.getElementById(`choice_5`).innerHTML =
+            Object.keys(jsQuestionList[i].choices[5]);
+        document.getElementById(`choice_6`).innerHTML =
+            Object.keys(jsQuestionList[i].choices[6]);
+
+        yield
+    }
+
+}
+
+const nextQ = nextQuestion()
+nextQ.next()
+
