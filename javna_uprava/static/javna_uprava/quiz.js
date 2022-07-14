da6a31a10e493397ba705666d8ed96141205d6eb
Ernad Osmic <ernadosmic@hotmail.com>
2022-07-14 16:10:13 +0200

    commit

--- javna_uprava/static/javna_uprava/quiz.js
+++ javna_uprava/static/javna_uprava/quiz.js
@@ -1,7 +1,12 @@
 console.log(jsQuestionList);
+console.log(Object.keys(jsQuestionList).length);
+let correct_answers_counter = 0;
+let incorrect_answers_counter = 0;
+let counter = 0;
 
 function* nextQuestion() {
-    let counter = 0;
+
+
     console.log(counter);
     for (i in jsQuestionList) {
         counter++;
@@ -22,7 +27,8 @@ function* nextQuestion() {
         // Create question text inside a created div
         const current_question_text = document.createElement("div");
         current_question_text.setAttribute("id", "current_question_text");
-        current_question_text.textContent = jsQuestionList[i].question;
+        current_question_text.textContent =
+            `${counter}. ${jsQuestionList[i].question}`;
         document
             .getElementById("current_question_box")
             .appendChild(current_question_text);
@@ -38,9 +44,9 @@ function* nextQuestion() {
         for (x in jsQuestionList[i].choices) {
             try {
                 const btn = document.createElement("button");
-                btn.setAttribute("id", `${i}_${x}`);
-                btn.setAttribute("onclick", "nextQuestionClick()");
-                btn.setAttribute("value", Object.values(jsQuestionList[i].choices[x]))
+                btn.setAttribute("id", `${i}${x}`);
+                btn.setAttribute("onclick", `nextQuestionClick(${Object.values(jsQuestionList[i].choices[x])}, ${i}${x})`);
+                btn.setAttribute("value", Object.values(jsQuestionList[i].choices[x]));
                 btn.textContent = Object.keys(jsQuestionList[i].choices[x]);
                 document.getElementById("current_choices_box").appendChild(btn);
             } catch (error) { }
@@ -53,8 +59,70 @@ function* nextQuestion() {
 const nextQ = nextQuestion();
 nextQ.next();
 
-async function nextQuestionClick() {
+function nextQuestionClick(value, id) {
+    // Grab the pressed button as "el"
+    let el = document.getElementById(id);
 
-    await
-        nextQ.next();
+    // Grab the element where the choices are
+    let current_choices_box = document.getElementById("current_choices_box");
+
+    // Grab and loop through the children of that element
+    let children = current_choices_box.childNodes;
+    children.forEach(button => {
+        // and setAttribute to disable
+        // so they cannot be clicked
+        button.setAttribute("disabled", "true");
+        // grab the child with value = true and color it green
+        if (button.getAttribute("value") === "true") {
+            button.setAttribute("class", "correct-answer");
+        }
+    })
+
+    // If the answer is correct, do this
+    if (value) {
+        correct_answers_counter++;
+        // If the answer is not correct, do this
+    } else {
+        el.setAttribute('class', 'incorrect-answer');
+        incorrect_answers_counter++
+    }
+
+    // wait a second
+    setTimeout(() => {
+        if (counter === Object.keys(jsQuestionList).length) {
+            console.log("IDEMO DALJE");
+            viewResults();
+        } else {
+            nextQ.next();
+        }
+
+    }, "2000");
+    console.log("Odmah");
 }
+
+function viewResults() {
+    try {
+        const elem = document.getElementById("current_question_box");
+        elem.parentNode.removeChild(elem);
+    } catch (error) { }
+
+    question_box = document.getElementById("question_box")
+
+    const correct_answers_result = document.createElement("div")
+    correct_answers_result.setAttribute("id", "correct_answers_result");
+    question_box.appendChild(correct_answers_result);
+    correct_answers_result.textContent = correct_answers_counter;
+
+    const incorrect_answers_result = document.createElement("div")
+    incorrect_answers_result.setAttribute("id", "incorrect_answers_result");
+    question_box.appendChild(incorrect_answers_result);
+    incorrect_answers_result.textContent = incorrect_answers_counter;
+
+}
+
+
+// Warning message if the user wants to leave the quiz
+// window.onbeforeunload = function () {
+//     return "Data will be lost if you leave the page, are you sure?";
+// };
+
