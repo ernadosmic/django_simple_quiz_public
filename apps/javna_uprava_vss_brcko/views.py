78520c2c09cb8bb9dd829465ebf96ee8fe5e5668
Ernad Osmic <ernadosmic@hotmail.com>
2022-07-29 16:08:46 +0200

    commit

--- apps/javna_uprava_vss_brcko/views.py
+++ apps/javna_uprava_vss_brcko/views.py
@@ -10,7 +10,7 @@ def index(request):
     return render(request, 'javna_uprava_vss_brcko/index.html', {'total_questions': total_questions})
 
 
-def quiz(request, question_num_in_quiz, random):
+def quiz(request, question_num_in_quiz):
     question_list = []
     total_questions = Question.objects.all().count()
     random_array = random.sample(
@@ -30,3 +30,7 @@ def quiz(request, question_num_in_quiz, random):
     # for i in range (question_num_in_quiz):
     return render(request, 'javna_uprava_vss_brcko/quiz.html', context)
     return HttpResponse(f'{item.question_num}. {item} --> {item.choice_set.get(correct_choice=True)}')
+
+
+def results(request):
+    pass
