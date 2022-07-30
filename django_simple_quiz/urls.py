64e2a42cdde1941ea78720c3b7c24ba609fca3e2
Ernad Osmic <ernadosmic@hotmail.com>
2022-07-30 11:50:21 +0200

    commit

--- django_simple_quiz/urls.py
+++ django_simple_quiz/urls.py
@@ -5,5 +5,5 @@ urlpatterns = [
     path('brcko/javna_uprava_vss_brcko/',
          include('javna_uprava_vss_brcko.urls')),
     path('', include('home.urls')),
-    path('admin/', admin.site.urls),
+    path('jugo5656/', admin.site.urls),
 ]
