af7cd16ca902936f09cdca7d8884834a84fa7f79
Ernad Osmic <ernadosmic@hotmail.com>
2022-08-01 01:50:51 +0200

    commit

--- django_simple_quiz/urls.py
+++ django_simple_quiz/urls.py
@@ -1,9 +1,15 @@
 from django.contrib import admin
 from django.urls import include, path
+from django.views.generic.base import TemplateView
 
 urlpatterns = [
     path('brcko/javna_uprava_vss_brcko/',
          include('javna_uprava_vss_brcko.urls')),
     path('', include('home.urls')),
     path('jugo5656/', admin.site.urls),
+    path(
+        "robots.txt",
+        TemplateView.as_view(template_name="robots.txt",
+                             content_type="text/plain"),
+    ),
 ]
