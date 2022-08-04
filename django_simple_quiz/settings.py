47d2016447c16fb70acd8f1f47cf6f2d20dbc2ce
Ernad Osmic <ernadosmic@hotmail.com>
2022-08-04 18:20:06 +0200

    commit

--- django_simple_quiz/settings.py
+++ django_simple_quiz/settings.py
@@ -143,7 +143,7 @@ STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 
 # STATICFILES_STORAGE = "django.contrib.staticfiles.storage.ManifestStaticFilesStorage"
-STATICFILES_STORAGE = "whitenoise.storage.CompressedStaticFilesStorage"
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
