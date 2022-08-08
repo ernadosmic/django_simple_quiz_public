64b5955621f2e2dcec12bd901eb0450512c214a2
Ernad Osmic <ernadosmic@hotmail.com>
2022-08-08 12:04:53 +0200

    commit

--- django_simple_quiz/settings.py
+++ django_simple_quiz/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'af86a214bfe582778a5fea636414afbbc747cf7af1ec0428'
+SECRET_KEY = '123'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
